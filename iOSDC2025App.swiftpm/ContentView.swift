206586ffb2fda9afdd6011f39355ebc8df70dc28
Yuki Yasoshima <yukiyasos@gmail.com>
2025-09-13 10:22:35 +0900

    add json

--- iOSDC2025App.swiftpm/ContentView.swift
+++ iOSDC2025App.swiftpm/ContentView.swift
@@ -2,11 +2,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List {
+            ForEach(timetable.days, id: \.id) { day in
+                Section {
+                    Text("TBD")
+                } header: {
+                    Text(day.title)
+                }
+            }
         }
     }
+}
+
+let timetable: Timetable = {
+    let url = Bundle.main.url(forResource: "iosdc2025_timetable", withExtension: "json")!
+    let data = try! Data(contentsOf: url)
+    let decoder = JSONDecoder()
+    decoder.keyDecodingStrategy = .convertFromSnakeCase
+    return try! decoder.decode(Timetable.self, from: data)
+}()
+
+struct Timetable: Codable {
+    let days: [Day]
+}
+
+struct Day: Codable {
+    let id: Int
+    let title: String
+    let sessions: [Session]
+}
+
+struct Session: Codable {
+    let id: Int
+    let title: String
+    let trackId: Int
 }
