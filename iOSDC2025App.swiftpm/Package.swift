7cb23c6ef28f7d20973551bde3fff379bf0c28ba
iosdc-dena <sokume2106@gmail.com>
2025-09-20 12:55:46 +0900

    _tak_hito_

--- iOSDC2025App.swiftpm/Package.swift
+++ iOSDC2025App.swiftpm/Package.swift
@@ -10,7 +10,7 @@ import AppleProductTypes
 let package = Package(
     name: "iOSDC2025App",
     platforms: [
-        .iOS("16.0")
+        .iOS("26.0")
     ],
     products: [
         .iOSApplication(
--- iOSDC2025App.swiftpm/View/ContentView.swift
+++ iOSDC2025App.swiftpm/View/ContentView.swift
@@ -3,55 +3,20 @@ import SwiftUI
 struct ContentView: View {
     @State private var searchText: String = ""
     var body: some View {
-        NavigationStack {
             TabView {
                 ForEach(timetable.days, id: \.id) { day in
-                    List {
-                        ForEach(filter(sessions: day.sessions, text: searchText), id: \.id) { session in
-                            NavigationLink {
-                                // TODO: iOSDCTrack の取得方法ちょっとダサいので任せた
-                                DetailView(track: timetable.tracks.track(id: session.trackId), session: session)
-                            } label: {
-                                HStack(alignment: .center) {
-                                    VStack(alignment: .leading, spacing: 8) {
-                                        HStack {
-                                            Text(session.startTime.dateString)
-                                            Text(timetable.tracks[session.trackId].name)
-                                        }
-                                        Text(session.title)
-                                            .font(.title)
-                                        // TODO: もっとおしゃれにしてください
-                                        if let speaker = session.speaker {
-                                            VStack(alignment: .leading, spacing: 4) {
-                                                Text(speaker.name)
-                                            }
-                                        }
-                                    }
-                                    .padding(4)
-                                    Spacer()
-                                    speakerImage(session: session)
-                                }
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                            }
+                    Tab(day.title, systemImage: "\(day.id - 1).circle") {
+                        NavigationStack {
+                            daylist(day: day)
                         }
-                    }
-                    .tabItem {
-                        Label(day.title, systemImage: "\(day.id - 1).circle")
+                        .searchable(text: $searchText, placement: .navigationBarDrawer)
                     }
                 }
-            }
-            .navigationTitle("iOSDC Time Table")
-            .toolbar {
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button {
-                        // TODO: - TBD
-                    } label: {
-                        Image(systemName: "menucard")
-                    }
+                Tab(role: .search) {
+                    searchView()
+                        .searchable(text: $searchText, placement: .navigationBarDrawer)
                 }
             }
-        }
-        .searchable(text: $searchText, placement: .navigationBarDrawer)
     }
     
     @ViewBuilder
@@ -75,6 +40,48 @@ struct ContentView: View {
         }
         return sessions.filter({ $0.title.contains(text) })
     }
+    
+    @ViewBuilder
+    func daylist(day: iOSDCDay) -> some View {
+        List {
+            ForEach(filter(sessions: day.sessions, text: searchText), id: \.id) { session in
+                NavigationLink {
+                    // TODO: iOSDCTrack の取得方法ちょっとダサいので任せた
+                    DetailView(track: timetable.tracks.track(id: session.trackId), session: session)
+                } label: {
+                    ContentCell(session: session)
+                }
+            }
+        }
+        .navigationTitle("iOSDC Time Table")
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    // TODO: - TBD
+                } label: {
+                    Image(systemName: "menucard")
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func searchView() -> some View {
+        NavigationStack {
+            List {
+                let sessions = timetable.days.compactMap(\.self).flatMap { $0.sessions }
+                ForEach(filter(sessions: sessions, text: searchText), id: \.id) { session in
+                    NavigationLink {
+                        // TODO: iOSDCTrack の取得方法ちょっとダサいので任せた
+                        DetailView(track: timetable.tracks.track(id: session.trackId), session: session)
+                    } label: {
+                        ContentCell(session: session)
+                    }
+                }
+            }
+            .navigationTitle("Search")
+        }
+    }
 }
 
 #Preview {
@@ -90,7 +97,46 @@ let timetable: IOSDC2025Timetable = {
     return try! decoder.decode(IOSDC2025Timetable.self, from: data)
 }()
 
-
+struct ContentCell: View {
+    let session: iOSDCSession
+    
+    var body: some View {
+        HStack(alignment: .center) {
+            VStack(alignment: .leading, spacing: 8) {
+                HStack {
+                    Text(session.startTime.dateString)
+                    Text(timetable.tracks[session.trackId].name)
+                }
+                Text(session.title)
+                    .font(.title)
+                // TODO: もっとおしゃれにしてください
+                if let speaker = session.speaker {
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text(speaker.name)
+                    }
+                }
+            }
+            .padding(4)
+            Spacer()
+            speakerImage(session: session)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    @ViewBuilder
+    func speakerImage(session: iOSDCSession) -> some View {
+        if let imageUrl = session.speaker?.imageUrl {
+            AsyncImage(
+                url: imageUrl,
+                content: { image in
+                    image.image?.resizable()
+                }
+            )
+            .frame(width: 60, height: 60)
+            .clipShape(Circle())
+        }
+    }
+}
 
 // MARK: -
 
